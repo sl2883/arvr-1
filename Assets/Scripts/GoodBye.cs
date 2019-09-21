df8d5c360304fd80690740a54819c47733b37245
Sunny Ladkani <54920880+sl2883@users.noreply.github.com>
2019-09-21 16:37:40 -0400

    Third part

---
+++ Assets/Scripts/GoodBye.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoodBye : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        StartCoroutine(LoadScene());
+    }
+
+
+    IEnumerator LoadScene()
+    {
+        yield return new WaitForSeconds(4);
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Scripts/StartGoldberg.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StartGoldberg : MonoBehaviour
+{
+    private Camera cameraC;
+
+    [SerializeField]
+    public TextMeshProUGUI textBox;
+
+    public string standByText = "Click Here";
+    public string startedText = "Good Luck, Sunny!";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraC = Camera.main;
+        textBox.text = standByText;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+           
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                textBox.text = startedText;
+                Interact(hit);
+                
+            }
+
+
+            Debug.Log("Pressed left click.");
+        }
+
+    }
+
+    void Interact(RaycastHit hit) 
+    {
+        Debug.Log("Hiting " + hit.collider.name);
+        if(hit.collider.name == "StartPlatform")
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
